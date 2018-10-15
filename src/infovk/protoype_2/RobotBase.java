64bf1d79632b0fec954c7e26599d47771d1f7109
kevin.iselborn@t-online.de <adMT2000>
2018-10-15 14:19:44 +0200

    Starting utils

--- src/infovk/protoype_2/RobotBase.java
+++ src/infovk/protoype_2/RobotBase.java
@@ -1,7 +1,52 @@
 package infovk.protoype_2;
 
 public class RobotBase extends SimpleRobot {
+    private double energyPowerFactor;
     public RobotBase() {
+        setEnergyPowerFactor(50);
+    }
 
+    public double getEnergyPowerFactor() {
+        return energyPowerFactor;
+    }
+
+    public void setEnergyPowerFactor(double energyPowerFactor) {
+        this.energyPowerFactor = energyPowerFactor;
+    }
+
+    @Override
+    public void run() {
+
+        if (behavior != null) {
+            behavior.start();
+        }
+        start();
+        execute();
+
+        while (true) {
+            if (behavior != null) {
+                behavior.execute();
+            }
+            loop();
+            execute();
+        }
+    }
+
+    protected void setAdjustToTurns() {
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        setAdjustRadarForRobotTurn(true);
+    }
+
+    protected void start() {
+
+    }
+
+    protected void loop() {
+
+    }
+
+    protected void fireRelativeToEnergy(double baseVal) {
+        fire(baseVal * getEnergy() / 50);
     }
 }
